ee14e33202a8ea4880881b6c7611b6a0f93c3e03
Peng.Li <nowind_lee@qq.com>
2016-10-05 14:32:11 +0800

    make it simple

--- hello.jsx
+++ hello.jsx
@@ -1,26 +1,11 @@
 import React, {Component} from 'react';
 
 class Hello extends Component {
-  constructor(props) {
-    // required if we want to invoke `this.`
-    super(props);
-    this.state = {
-      username: ''
-    };
-  }
 
   render() {
-    return <div>
-      <h1>Hello {this.state.username}</h1>
-      <input type="text"
-             onChange={this.handleNewUsername.bind(this)}
-      />
-    </div>;
+    return <div>Hello world!</div>;
   }
 
-  handleNewUsername(event) {
-    this.setState({username: event.target.value});
-  }
 }
 
 export default Hello;
